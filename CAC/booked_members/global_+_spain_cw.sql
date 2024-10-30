e9d8cd847e35d58d7c60952db3d9f90cbc82ab27
jbebetterfly <jbezanilla@betterfly.com>
2024-10-30 11:17:43 -0300

    added cw booked members and structure to cac folder

--- CAC/booked_members/global_+_spain_cw.sql
+++ CAC/booked_members/global_+_spain_cw.sql
@@ -1,5 +1,51 @@
-with
-    last_date as (
+with global as (SELECT 
+  EXTRACT(MONTH FROM hs_closed_date) AS closed_date_month,
+  EXTRACT(YEAR FROM hs_closed_date) AS closed_date_year,
+  hs_betterfly_country,
+  CASE
+  WHEN lower(hs_company_size) = 'micro' THEN 'SME'
+  WHEN lower(hs_company_size) = 'sme' THEN 'SME'
+  WHEN lower(hs_company_size) = 'corporate' THEN 'Enterprise'
+  WHEN lower(hs_company_size) = 'enterprise' THEN 'Enterprise'
+  WHEN lower(hs_company_size) = 'big enterprise' THEN 'Enterprise'
+  END AS bu,
+  hs_number_of_employees,
+  hs_closed_amount_in_company_currency
+  
+  FROM `btf-source-of-truth.acquisition.deals`
+
+  WHERE hs_closed_date >= '2024-01-01'
+  AND hs_deal_Stage = 'Cierres ganados'
+  --AND UPPER(hs_company_name) NOT LIKE '%COLSUBSIDIO%'
+  AND (lower(hs_product_modules) not in (
+       'gift cards',
+       'gift cards 🇨🇱',
+       'flexben',
+       'flexoh',
+       'seu vale',
+       'flexoh - poliza de seguro',
+       'poliza de seguro'
+      )
+    OR hs_product_modules IS null)
+    AND lower(hs_deal_name) NOT LIKE '%test%'
+    AND lower(hs_deal_name) NOT LIKE '%prueba%'
+    AND lower(hs_deal_name) NOT LIKE '%wallet%'
+    AND (lower(hs_development_type) NOT LIKE '%piloto%' or hs_development_type is null)
+    AND (lower(hs_development_type) NOT LIKE '%reactivate%' or hs_development_type is null)
+    AND (lower(hs_development_type) NOT LIKE '%retention%' or hs_development_type is null)
+    AND (lower(hs_value_proposition) NOT LIKE '%customer_engagement%' or hs_value_proposition is null)
+    AND (lower(hs_partnership_demand_name) NOT LIKE '%colsubsidio%' or hs_partnership_demand_name is null)
+    and lower(hs_deal_pipeline) in ('pipeline de ventas','pipeline enterprise')
+    and lower(hs_value_proposition) not in ('affinity') 
+    and lower(hs_betterfly_country) not in ('spain')
+  
+  ORDER BY hs_closed_date,
+  hs_betterfly_country,
+  hs_company_size,
+  hs_number_of_employees,
+  hs_closed_amount_in_company_currency),
+
+    last_date_esp as (
         select
             hs_deal_id,
             max(hs_last_modified_date) as last_date
@@ -21,12 +67,11 @@ with
   WHEN lower(hs_company_size) = 'big enterprise' THEN 'Enterprise'
   END AS bu,
   hs_number_of_employees,
-  hs_closed_amount_in_company_currency,
-  hs_deal_stage
+  hs_closed_amount_in_company_currency
         FROM
             `btf-source-of-truth.acquisition.deals` as ad
-            inner join last_date as ld on ad.hs_deal_id = ld.hs_deal_id
-            and ad.hs_last_modified_date = ld.last_date
+            inner join last_date_esp as ld on ad.hs_deal_id = ld.hs_deal_id
+            AND ad.hs_last_modified_date = ld.last_date
         WHERE
             hs_closed_date >= '2024-01-01'
             and hs_created_date <= current_date() -1
@@ -46,7 +91,7 @@ with
 
             ORDER by hs_closed_date),
             espana_1 AS (
-            SELECT 
+            SELECT DISTINCT
             closed_date_month,
             closed_date_year,
             hs_betterfly_country,
@@ -57,4 +102,12 @@ with
 
             ORDER BY 
             closed_date_year, closed_date_month)
-            SELECT * from espana_1
+          
+SELECT * from global
+UNION ALL
+SELECT * from espana_1
+
+ORDER BY 
+closed_date_year,
+closed_date_month,
+hs_betterfly_country
