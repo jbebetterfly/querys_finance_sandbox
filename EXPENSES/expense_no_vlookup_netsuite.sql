cae30902dcc18b6238531bc2d29163e51f17c955
jbebetterfly <jbezanilla@betterfly.com>
2024-11-14 15:21:32 -0300

    Fix stuff clasificación gastos no vlookup

--- EXPENSES/expense_no_vlookup_netsuite.sql
+++ EXPENSES/expense_no_vlookup_netsuite.sql
@@ -3,26 +3,29 @@ SELECT
 gastos_base.expense_id,
 gastos_base.transaction_id,
 gastos_base.document_id,
-gastos_base.full_date,
+CAST(gastos_base.full_date as DATETIME) as full_date,
 gastos_base.accounting_account_id,
 gastos_base.accounting_account_name,
-null as management_account_id,
+CAST(null as string) as management_account_id,
 relacion_cuentas.management_account_name,
 relacion_cuentas.management_account_classification as mgmt_account_classification,
 relacion_cuentas.management_account_subclassification as mgmt_account_subclasification,
-gastos_base.accounting_department_id,
-gastos_base.accounting_department_name,
-gastos_base.accounting_country_id,
-gastos_base.accounting_country_name,
+gastos_base.accounting_department_id as accounting_cost_center_dim1_id,
+gastos_base.accounting_department_name as accounting_cost_center_dim1_name,
+gastos_base.accounting_country_id as accounting_cost_center_dim2_id,
+gastos_base.accounting_country_name as accounting_cost_center_dim2_name,
 relacion_cecos.management_department,
 classification_department.department_classification,
-gastos_base.product_id,
-gastos_base.product_name,
+gastos_base.product_id as project_id,
+gastos_base.product_name as project_name,
 gastos_base.transaction_detail,
 relacion_cecos_dim2.service_country,
 gastos_base.legal_entity_country,
 gastos_base.legal_entity_name,
-gastos_base.segment_name as holding_segment,
+CASE
+WHEN gastos_base.segment_name IS NULL OR gastos_base.segment_name = '' THEN 'Sin holding'
+ELSE gastos_base.segment_name
+END AS holding_segment,
 relacion_cecos.sales_channel,
 relacion_cecos.revenue_stream,
 gastos_base.business_partner_id,
@@ -35,11 +38,11 @@ fx.Value as value_fx_bdg,
 safe_divide(gastos_base.value_lc, fx.Value) as value_usd_bdg,
 gastos_base.version,
 gastos_base.subversion,
-gastos_base.payment_date,
+CAST(gastos_base.payment_date AS DATETIME) as payment_date,
 gastos_base.source,
 CASE
-WHEN gastos_base.business_partner_name = 'ENTERPRISE' THEN 'Enterprise'
-WHEN gastos_base.business_partner_name = 'SMB' THEN 'SMB'
+WHEN gastos_base.segment_name = 'ENTERPRISE' THEN 'Enterprise'
+WHEN gastos_base.segment_name = 'SME' THEN 'SMB'
 ELSE null END AS business_unit,
 EXTRACT(MONTH FROM gastos_base.full_date) as month,
 EXTRACT(YEAR FROM gastos_base.full_date) as year
@@ -63,3 +66,4 @@ LEFT JOIN `btf-finance-sandbox.Budget.Currency_conversion_bdg` fx
 ON CAST(EXTRACT(YEAR FROM gastos_base.full_date) as string) = fx.Year
 AND UPPER(gastos_base.currency) = UPPER(fx.Currency)
 
+order by management_department
